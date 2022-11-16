390b29027ca72e5254bcf665f1786870750748aa
DudaCampello <114358060+DudaCampello@users.noreply.github.com>
2022-11-16 14:33:59 -0300

    inicial

---
+++ src/main/java/one/digitalinnovation/cep/service/ClienteService.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.cep.service;
+
+import one.digitalinnovation.cep.model.Cliente;
+
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
